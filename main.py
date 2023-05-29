2f0f699723b7feb226f7a2b3836c04f8c25551b4
Ishan-sa <ishansach19@gmail.com>
2023-05-29 15:22:21 -0700

    some shit

--- main.py
+++ main.py
@@ -8,4 +8,12 @@ def hello_world():
     return render_template("index.html")
 
 
+@app.route('/api', methods=['GET', 'POST'])
+def qa():
+    if request.method == 'POST':
+        data = {"result": "hey this is a test result"}
+        return jsonify(data)
+    return render_template("index.html")
+
+
 app.run(debug=True)
